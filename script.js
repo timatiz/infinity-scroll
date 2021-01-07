53f8b62b959f3a4023a930fb58817185df0c038f
timatiz <58471927+timatiz@users.noreply.github.com>
2021-01-07 14:59:16 +0100

    initial load fixed

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ let totalImages = 0
 let photosArray = []
 
 //Unsplash API
-const count = 30
+let count = 5
 const apiKey = "NOvEdcRqJqZ3W5OYfRaDIma97lK8U6-KYrTx_MD_-lc"
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}&`
+let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}&`
 
 // Check if all images were loaded
 function imageLoaded() {
@@ -17,6 +17,7 @@ function imageLoaded() {
   if (imagesLoaded === totalImages) {
     ready = true
     loader.hidden = true
+    count = 30
   }
 }
 
